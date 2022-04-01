5af6a94cbc9473e8954a32ce0ec16c5bd8e5019e
Waizul Haque <waizul@gmail.com>
2022-04-01 17:02:48 +0600

    next starter

---
+++ components/Navbar.jsx
@@ -0,0 +1,11 @@
+import styles from "../styles/Navbar.module.css";
+
+const Navbar = () => {
+  return (
+    <header className={styles.container}>
+      <nav className={styles.nav}>Navbar</nav>
+    </header>
+  );
+};
+
+export default Navbar;
